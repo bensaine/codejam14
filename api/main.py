6324eccd0bf0c66a02bb744cb78acf1ee66cb03d
Ben <bensaine09@gmail.com>
2024-11-22 21:36:01 -0500

    api: add /crimes endpoints

--- api/main.py
+++ api/main.py
@@ -1,11 +1,22 @@
 from flask import Flask, jsonify, request
+from json import loads
 
+DATA = "./data/"
 app = Flask(__name__)
+
+# load data from json file
+with open(DATA + "crimes.json") as f:
+    crimes_data = loads(f.read())
 
 
 @app.route("/", methods=["GET"])
 def helloworld():
     return jsonify({"message": "Hello World"})
+
+
+@app.route("/crimes", methods=["GET"])
+def crimes():
+    return jsonify(crimes_data)
 
 
 if __name__ == "__main__":
