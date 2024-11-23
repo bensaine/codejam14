c5053ff198e23830e48b34f8097cca9bd8eb8e3a
James Liang <james.liang@umontreal.ca>
2024-11-22 21:11:46 -0500

    added heatmap and geojson

--- web/src/components/Map.jsx
+++ web/src/components/Map.jsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import { MapContainer, TileLayer, GeoJSON, Marker } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-import L, { geoJson } from 'leaflet';
-import 'leaflet-defaulticon-compatibility';
-import HeatmapLayer from 'react-leaflet-heatmap-layer-v3'
-import geoJsonData from '../assets/actes-criminels_short.json'
+import React from "react";
+import { MapContainer, TileLayer, GeoJSON, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import L, { geoJson } from "leaflet";
+import "leaflet-defaulticon-compatibility";
+import {HeatmapLayer} from "react-leaflet-heatmap-layer-v3";
+import { addressPoints } from '../assets/realworld.10000'
+import geoJsonData from "../assets/actes-criminels_short.json";
 const MapView = ({ geojsonData }) => {
-    const position = [45.514, -73.573]
-    const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    return (
-        <MapContainer 
-        preferCanvas={true} renderer={L.canvas()}
-        center={position} zoom={11} style={{ height: "100%", width: '100%' }}>
-            <TileLayer
-                url={url}
-                maxZoom={20}
-                minZoom={0}
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            
-            <Marker position={position}/>
-            <GeoJSON data={geoJsonData}> </GeoJSON>
-        </MapContainer>
-    );
+  const position = [45.514, -73.573];
+  const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+  return (
+    <MapContainer
+      preferCanvas={true}
+      renderer={L.canvas()}
+      center={position}
+      zoom={11}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <GeoJSON data={geoJsonData}> </GeoJSON>
+      <Marker position={position} />
+<HeatmapLayer
+            fitBoundsOnLoad
+            fitBoundsOnUpdate
+            points={addressPoints}
+            longitudeExtractor={m => m[1]}
+            latitudeExtractor={m => m[0]}
+            intensityExtractor={m => parseFloat(m[2])} />
+      <TileLayer
+        url={url}
+        maxZoom={20}
+        minZoom={0}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+    </MapContainer>
+  );
 };
 
 export default MapView;
