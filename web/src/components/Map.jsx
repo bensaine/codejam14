01913615bcb4302097bac7a19c71dbf2ae41048e
James Liang <james.liang@umontreal.ca>
2024-11-22 20:28:08 -0500

    fixed map

--- web/src/components/Map.jsx
+++ web/src/components/Map.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { MapContainer, TileLayer, GeoJSON, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
-import L from 'leaflet';
+import L, { geoJson } from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 import geoJsonData from '../assets/actes-criminels_short.json'
 const MapView = ({ geojsonData }) => {
+    const position = [45.514, -73.573]
     return (
-        <MapContainer center={[45.514, -73.573]} zoom={11} style={{ height: "90", width: '90vw' }}>
+        <MapContainer center={position} zoom={11} style={{ height: "100vh", width: '100vw' }}>
             <TileLayer
-            url={'https://{s}.basemaps.cartocdn.com/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png')}
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+            <Marker position={position}/>
+            <GeoJSON data={geoJsonData}>
+
+            </GeoJSON>
         </MapContainer>
     );
 };
