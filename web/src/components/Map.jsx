759c46e78b2e23f6b4a966ece077a926e4fb6238
Ben <bensaine09@gmail.com>
2024-11-22 22:11:48 -0500

    FORMAT

--- web/src/components/Map.jsx
+++ web/src/components/Map.jsx
@@ -1,40 +1,39 @@
-import React from "react";
-import { MapContainer, TileLayer, GeoJSON, Marker } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import L, { geoJson } from "leaflet";
-import "leaflet-defaulticon-compatibility";
-import {HeatmapLayer} from "react-leaflet-heatmap-layer-v3";
-import geoJsonData from "../assets/actes-criminels.json";
+import React from 'react'
+import { MapContainer, TileLayer, GeoJSON, Marker } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+import L, { geoJson } from 'leaflet'
+import 'leaflet-defaulticon-compatibility'
+import { HeatmapLayer } from 'react-leaflet-heatmap-layer-v3'
+import geoJsonData from '../assets/actes-criminels.json'
 const MapView = ({ geojsonData }) => {
-  const position = [45.514, -73.573];
-  const url = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png";
-  const addressPoints = geoJsonData['features'].map(el => {
-    return [el['properties']['LATITUDE'], el['properties']['LONGITUDE'], 0.3]
-  }
-  )
-  return (
-    <MapContainer
-      preferCanvas={true}
-      renderer={L.canvas()}
-      center={position}
-      zoom={20}
-      style={{ height: "100%", width: "100%" }}
-    >
-      <Marker position={position} />
-        <HeatmapLayer
-            points={addressPoints}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => parseFloat(m[2])} />
-      
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.carto.com/">CARTO</a> contributors'
-      />
+	const position = [45.514, -73.573]
+	const url = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+	const addressPoints = geoJsonData['features'].map((el) => {
+		return [el['properties']['LATITUDE'], el['properties']['LONGITUDE'], 0.3]
+	})
+	return (
+		<MapContainer
+			preferCanvas={true}
+			renderer={L.canvas()}
+			center={position}
+			zoom={20}
+			style={{ height: '100%', width: '100%' }}
+		>
+			<Marker position={position} />
+			<HeatmapLayer
+				points={addressPoints}
+				longitudeExtractor={(m) => m[1]}
+				latitudeExtractor={(m) => m[0]}
+				intensityExtractor={(m) => parseFloat(m[2])}
+			/>
 
-    </MapContainer>
-  );
-};
+			<TileLayer
+				url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+				attribution='&copy; <a href="https://www.carto.com/">CARTO</a> contributors'
+			/>
+		</MapContainer>
+	)
+}
 
-export default MapView;
+export default MapView
