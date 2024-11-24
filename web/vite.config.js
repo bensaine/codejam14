c2f5dbfec2bf7e2f6496ae36fa2c8dc5604796b3
Ben <bensaine09@gmail.com>
2024-11-24 03:40:49 -0500

    commit

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,37 +1,40 @@
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
 
-    manifest: {
-      name: 'StrideSafe',
-      short_name: 'StrideSafe',
-      description: 'StrideSafe',
-      theme_color: '#ffffff',
-    },
+			manifest: {
+				name: 'StrideSafe',
+				short_name: 'StrideSafe',
+				description: 'StrideSafe',
+				theme_color: '#ffffff',
+			},
 
-    workbox: {
-      globPatterns: ['**/*.{js,json,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-      maximumFileSizeToCacheInBytes: 10000000
-    },
+			workbox: {
+				globPatterns: ['**/*.{js,json,css,html,svg,png,ico}'],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+				maximumFileSizeToCacheInBytes: 10000000,
+			},
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+			devOptions: {
+				enabled: false,
+				navigateFallback: 'index.html',
+				suppressWarnings: true,
+				type: 'module',
+			},
+		}),
+	],
 })
