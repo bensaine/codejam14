aa5bf0d3454b905208e1523b3d1df3a81e94bd7f
Shtrust <shoaibfoot@gmail.com>
2024-11-24 06:07:57 -0500

    womp womp

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.jsx'
@@ -8,54 +8,60 @@ import MapView from './components/Map.jsx'
 import { APIProvider } from '@vis.gl/react-google-maps'
 import LocationProvider from './components/LocationProvider.jsx'
 import Theme from './components/Theme.jsx'
+import { LocationContext } from './contexts/LocationContext.jsx'
+import Directions from './components/Directions.jsx'
 
 function App() {
 	const [theme, setTheme] = useState('rastertiles/voyager')
 	const [isOpenHeatmap, setIsOpenHeatmap] = useState(true)
+	const { directions } = useContext(LocationContext)
+	const [showDirections, setShowDirections] = useState(false)
 
 	return (
 		<APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-			<LocationProvider>
-				<div style={{ width: '100vw', height: '100vh' }}>
-					<div className="mapContainer">
-						<div
-							style={{
-								position: 'absolute',
-								top: 0,
-								left: 0,
-								right: 0,
-								display: 'flex',
-								justifyContent: 'center',
-								alignItems: 'center',
-								padding: '2rem',
-								zIndex: 1000,
-							}}
-						>
-							<SearchPlaces></SearchPlaces>
-						</div>
-						<MapView theme={theme} isOpenHeatmap={isOpenHeatmap} />
+			<div style={{ width: '100vw', height: '100vh' }}>
+				<div className="mapContainer">
+					<div
+						style={{
+							position: 'absolute',
+							top: 0,
+							left: 0,
+							right: 0,
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
+							padding: '2rem',
+							zIndex: 1000,
+						}}
+					>
+						<SearchPlaces
+							toggleDirections={() => setShowDirections(!setShowDirections)}
+						></SearchPlaces>
+
+						{directions && <Directions directions={directions}></Directions>}
 					</div>
-					<div className="themeContainer">
-						<div>
-							<Theme setTheme={setTheme} />
-						</div>
-					</div>
-					<div className="map-background">
-						<div className="map-background__wrap">
-							Heatmap
-							<input
-								type="checkbox"
-								id="toggle"
-								checked={isOpenHeatmap}
-								onChange={(e) => {
-									setIsOpenHeatmap(e.target.checked)
-								}}
-							/>
-						</div>
-					</div>
-					<PWABadge />
+					<MapView theme={theme} isOpenHeatmap={isOpenHeatmap} />
 				</div>
-			</LocationProvider>
+				<div className="themeContainer">
+					<div>
+						<Theme setTheme={setTheme} />
+					</div>
+				</div>
+				<div className="map-background">
+					<div className="map-background__wrap">
+						Heatmap
+						<input
+							type="checkbox"
+							id="toggle"
+							checked={isOpenHeatmap}
+							onChange={(e) => {
+								setIsOpenHeatmap(e.target.checked)
+							}}
+						/>
+					</div>
+				</div>
+				<PWABadge />
+			</div>
 		</APIProvider>
 	)
 }
